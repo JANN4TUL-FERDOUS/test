8658cc923e4b3fea6c4515506e8529d4f6a8fc68
Jannatul Ferdous <127871721+JANN4TUL-FERDOUS@users.noreply.github.com>
2023-06-21 16:03:29 +0600

    Add files via upload

---
+++ insertForm.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>	
+	<center>
+		<h1>Insert Form</h1>
+		<form method="POST" action="insertData.php">
+
+			<label>Name</label>
+			<input type="text" placeholder="Enter name" name="f_name"> <br> <br>
+			<label>Designation</label>
+			<input type="text" placeholder="Enter Designation" name="f_designation"> <br> <br>
+			<label>phone</label>
+			<input type="text"  placeholder="Enter email" name="f_phone" > <br> <br>
+			<label>Date of Birth</label>
+			<input type="date"  placeholder="Enter DoB" name="f_dob" > <br> <br>
+			<input type="submit" value="INSERT">
+		</form>
+	</center>	
+	
+</body>
+</html>
